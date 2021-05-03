a704a91c0e478a87d36587bb82c7d33418b05288
NastyaBordun <bordun.2002@mail.ru>
2021-05-03 23:50:05 +0300

    Delete client directory

--- client/src/main/java/ApplicationClient.java
+++
@@ -1,9 +0,0 @@
-import utils.ClientLauncher;
-
-public class ApplicationClient {
-    public static void main(String[] args){
-        ClientLauncher clientLauncher = new ClientLauncher();
-        clientLauncher.launchClient();
-
-    }
-}
